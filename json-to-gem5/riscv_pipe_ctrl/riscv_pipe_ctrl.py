43784e157d20e2f5c925ee09ab106415a9c2f085
Zirong Guo <53646903+ZirongGuo@users.noreply.github.com>
2026-06-27 06:43:22 +0800

    Delete json-to-gem5/riscv_pipe_ctrl directory

--- json-to-gem5/riscv_pipe_ctrl/riscv_pipe_ctrl.py
+++
@@ -1,7 +0,0 @@
-from m5.params import *
-from m5.SimObject import SimObject
-
-class RiscvPipeCtrl(SimObject):
-    type = 'RiscvPipeCtrl'
-    cxx_header = "generators/riscv_pipe_ctrl/riscv_pipe_ctrl.hh"
-    cxx_class = "gem5::RiscvPipeCtrl"
