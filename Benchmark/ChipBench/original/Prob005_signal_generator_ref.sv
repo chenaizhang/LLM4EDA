406678b20a37bcaa7fab721fa3d4a74865249b7a
Zirong Guo <53646903+ZirongGuo@users.noreply.github.com>
2026-06-28 10:00:42 +0800

    Add files via upload

---
+++ Benchmark/ChipBench/original/Prob005_signal_generator_ref.sv
@@ -0,0 +1,48 @@
+
+module RefModule(
+	input clk,
+	input rst_n,
+	input [1:0] wave_choice,
+	output reg [4:0]wave
+	);
+
+    reg [4:0] cnt;
+    reg flag;
+    
+  	// Counter control in square wave mode
+    always@(posedge clk or negedge rst_n) begin
+        if(~rst_n)
+            cnt <= 0;
+        else
+            cnt <= wave_choice!=0 ? 0:
+                   cnt        ==19? 0:
+                   cnt + 1;
+    end
+    
+  	// Flag control in triangle wave mode
+    always@(posedge clk or negedge rst_n) begin
+        if(~rst_n)
+            flag <= 0;
+        else
+            flag <= wave_choice!=2 ? 0:
+                    wave       ==1 ? 1:
+                    wave       ==19? 0:
+                    flag;
+    end
+    
+  
+  	// Update wave signal
+    always@(posedge clk or negedge rst_n) begin
+        if(~rst_n) 
+            wave <= 0;
+        else 
+            case(wave_choice)
+                0      : wave <= cnt == 9? 20    : 
+                                 cnt ==19? 0     :
+                                 wave;
+                1      : wave <= wave==20? 0     : wave+1;
+                2      : wave <= flag==0 ? wave-1: wave+1;
+                default: wave <= 0;
+            endcase
+    end
+endmodule
